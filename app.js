9493046e58747c3e87426a168827ca50497a6ebb
eobaah <eobaah@gmail.com>
2018-03-24 18:46:45 -0700

    adds logo to landing page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Eugene!'));
+app.set('view engine', 'html');
+app.use(express.static(`${__dirname}/public`));
+app.set('views', path.resolve(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 
-app.listen(process.env.PORT || 8080);
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 8080, () => console.log('app is running on port 8080'));
